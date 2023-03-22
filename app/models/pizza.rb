4f924a0a89b21abb0eba9837e394ebfcbdb5b609
MakenaG <makenaglory44@gmail.com>
2023-03-22 16:37:33 +0300

    controller update

--- app/models/pizza.rb
+++ app/models/pizza.rb
@@ -3,7 +3,6 @@ class Pizza < ApplicationRecord
   has_many :restaurants, through: :restaurant_pizzas
 
     # Add some validations for the pizza attributes
-    validates :name, presence: true, uniqueness: true
-    validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
-    validates :toppings, presence: true
+    # validates :name, presence: true, uniqueness: true
+    # validates :toppings, presence: true
 end
