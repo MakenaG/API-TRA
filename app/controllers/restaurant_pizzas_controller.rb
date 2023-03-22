0b74d9f996f70c9c12a71a45c9cb4c20c53f02cf
MakenaG <makenaglory44@gmail.com>
2023-03-22 16:26:00 +0300

    controller update

--- app/controllers/restaurant_pizzas_controller.rb
+++ app/controllers/restaurant_pizzas_controller.rb
@@ -1,17 +1,18 @@
 class RestaurantPizzasController < ApplicationController
-    # define the create action that responds to POST /restaurant_pizza 
-    def create 
-      # create a new restaurant pizza with params and assign it to an instance variable @restaurant pizza 
-      @restaurant pizza = RestaurantPizza.new(params.require(:price).permit(:pizza_id, :restuarant_id))
-  
-      # if @restuarant pizza is saved successfully 
-      if @restuarant pizza.save 
-        # render its associated pizza as JSON data with only id name and ingredients attributes 
-        render json: @restuarant pizza.pizza.as_json(only: [:id, :name, :ingredients])
-      else 
-        # otherwise render an error message as JSON data with status code 422 (unprocessable entity) 
-        render json: { errors: @restuarant pizza.errors.full_messages }, status: :unprocessable_entity  
-      end  
-     end   
+  # define the create action that responds to POST /restaurant_pizzas
+  def create 
+    # create a new restaurant pizza with params and assign it to an instance variable @restaurant_pizza
+    @restaurant_pizza = RestaurantPizza.new(params.require(:restaurant_pizza).permit(:pizza_id, :restaurant_id))
+
+    # if @restaurant_pizza is saved successfully
+    if @restaurant_pizza.save 
+      # render its associated pizza as JSON data with only id, name, and ingredients attributes 
+      render json: @restaurant_pizza.pizza.as_json(only: [:id, :name, :ingredients])
+    else 
+      # otherwise render an error message as JSON data with status code 422 (unprocessable entity)
+      render json: { errors: @restaurant_pizza.errors.full_messages }, status: :unprocessable_entity  
+    end  
   end
+end
+
   
